4565b02c98b63e1b18ad24f8d4d8cd16b307bbb2
AlexBazovkin <alexbaz@vk.com>
2022-09-03 13:24:20 +0400

    Не то дз (((

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -1,5 +1,4 @@
 from collections import Counter
-
 from django.shortcuts import render
 
 # Для отладки механизма ab-тестирования используйте эти счетчики
@@ -12,6 +11,10 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+
+    result = request.GET.get('from-landing')
+    counter_click.update([result])
+
     return render(request, 'index.html')
 
 
@@ -20,13 +23,33 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
-    return render(request, 'landing.html')
+    # return render(request, 'landing.html')
+
+    result = request.GET.get('ab-test-arg')
+    counter_show.update([result])
+
+    if result == 'test':
+        return render(request, 'landing_alternate.html')
+
+    if result == 'original':
+        return render(request, 'landing.html')
 
 
 def stats(request):
     # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
     # Для вывода результат передайте в следующем формате:
+
+    if counter_show['test'] != 0:
+        count_test = round(counter_click['test'] / counter_show['test'], 1)
+    else:
+        count_test = 'Не было показов'
+
+    if counter_show['original'] != 0:
+        count_original = round(counter_click['original'] / counter_show['original'], 1)
+    else:
+        count_original = 'Не было показов'
+
     return render(request, 'stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': count_test,
+        'original_conversion': count_original,
     })
